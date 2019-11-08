df3a8e8b127fed33d385e7dc9e0931ed11f4ed34
sauravmaropost <saurav.kumar@maropost.com>
2019-11-08 16:28:24 +0530

    jwt work

---
+++ app/auth.go
@@ -0,0 +1,52 @@
+package app
+
+import (
+	"JSMPJ_jwt_final/validate"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+var JwtAuthentication = func(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("We are in middle ware")
+		notAuth := map[string]bool{"/login": true, "/account/verify/uid": true, "/account/user": true}
+		if !notAuth[r.URL.Path] {
+			if !VerifyToken(r) {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte("Invalid JWT Token"))
+				return
+			}
+		}
+
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
+}
+
+func VerifyToken(req *http.Request) bool {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("execption handled in VerifyToken : ", err)
+		}
+	}()
+
+	//expecting `jwt token ` from Header key Authorization
+	token := req.Header.Get("Authorization")
+
+	// removing Bearer from the JWT token
+	token = strings.TrimSpace(strings.Replace(token, "Bearer", "", -1))
+
+	if token == "" {
+		log.Println("empty JWT Token Header key Authorization : ", token)
+		return false
+	}
+
+	status, err := validate.ValidateJWTToken(token)
+	if err != nil {
+		return false
+	}
+	return status
+}
